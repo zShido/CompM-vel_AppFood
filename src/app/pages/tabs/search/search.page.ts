244ae8f92638785637c555e2d23a99162904abfa
zShido <caubyjeth13@gmail.com>
2024-02-15 11:20:59 -0300

    Tela de Search e Account feitas, somente front

--- src/app/pages/tabs/search/search.page.ts
+++ src/app/pages/tabs/search/search.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-search',
@@ -6,10 +6,79 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.page.scss'],
 })
 export class SearchPage implements OnInit {
-
-  constructor() { }
+    @ViewChild ('searchInput') sInput;
+    allRestaurants : any [] = [];
+    restaurants : any [] = [];
+    query: any;
+    isLoading; boolean = false;
+    model: any = {
+      icon: 'search-outline',
+      title: 'Nenhum estabeleciomento encontrado.'
+    }
+  constructor() {
+    setTimeout (()=>{this.sInput.setFocus()},500);
+   }
 
   ngOnInit() {
+    this.allRestaurants = [
+      {
+        cover: 'assets/imgs/img1.jpeg',
+        name: 'Dom Henrique 1',
+        short_name: 'domhenrique1',
+        cuisines:[
+          'Almoço',
+          'Café',
+          'Hamburguer'
+        ],
+        rating: 5,
+        delivery_time: 25,
+        distance: 2.5,
+        price: 100
+      },
+      {
+        cover: 'assets/imgs/img2.jpeg',
+        name: 'Dom Henrique 2',
+        short_name: 'domhenrique2',
+        cuisines:[
+          'Almoço',
+          'Café',
+          'Hamburguer'
+        ],
+        rating: 5,
+        delivery_time: 25,
+        distance: 2.5,
+        price: 100
+      },
+      {
+        cover: 'assets/imgs/img3.jpeg',
+        name: 'Dom Henrique 3',
+        short_name: 'domhenrique3',
+        cuisines:[
+          'Almoço',
+          'Café',
+          'Hamburguer'
+        ],
+        rating: 5,
+        delivery_time: 25,
+        distance: 2.5,
+        price: 100
+      },
+    ];
+  }
+
+  async onSearchChange(event){
+    this.query = event.detail.value.toLowerCase();
+    this.restaurants = [];
+    if(this.query.length > 0){
+      this.isLoading = true;
+      setTimeout(async()=>{
+        this.restaurants = await this.allRestaurants.filter((element : any) => {
+          return element.short_name.includes(this.query)
+        })
+        console.log(this.restaurants);
+        this.isLoading = false;
+      }, 3000)
+    }
   }
 
 }
